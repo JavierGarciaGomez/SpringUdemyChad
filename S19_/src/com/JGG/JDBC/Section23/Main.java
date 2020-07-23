5068055ee864fae9e75116e161c5c5ad0b134d7b
JGG <javieron.garcia@gmail.com>
2020-07-23 20:23:59 +0200

    Section 23 OneToOne. Part 1

---
+++ S19_/src/com/JGG/JDBC/Section23/Main.java
@@ -0,0 +1,129 @@
+package com.JGG.JDBC.Section23.Section21exercise;
+
+import com.JGG.JDBC.Section21.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner;
+    private static SessionFactory factory;
+    private static Session session;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        // Create session factory
+        factory = new Configuration().configure("hibernate21_22.cfg.xml")
+                .addAnnotatedClass(Student.class).buildSessionFactory();
+
+        int selection = 0;
+        do {
+            printOptions();
+            selection = Integer.parseInt(scanner.nextLine());
+            switch (selection) {
+                case 5:
+                    createEmployee();
+                    break;
+
+                case 6:
+                    retrieveEmployee();
+                    break;
+
+                case 7:
+                    queryEmployees();
+                    break;
+                    
+                case 8:
+                    deleteEmployee();
+                    break;
+                    
+                default:
+                    break;
+                    
+
+            }
+        } while (selection != 9);
+
+
+    }
+
+
+
+    private static void printOptions() {
+        System.out.println(
+                "Escoge una de las siguientes opciones: " +
+                        "\n5. Create a new employee" +
+                        "\n6. Retrieve an employee with primary key" +
+                        "\n7. Query objects to find employees for a given company" +
+                        "\n8. Delete an object by primary key" +
+                        "\n9. Exit");
+    }
+
+    private static void createEmployee() {
+        System.out.println("CREATE A NEW EMPLOYEE");
+
+        System.out.println("Insert the first name");
+        String firstName = scanner.nextLine();
+        System.out.println("Insert the last name");
+        String lastName = scanner.nextLine();
+        System.out.println("Insert the company name");
+        String company = scanner.nextLine();
+
+        session = factory.getCurrentSession();
+        com.JGG.JDBC.Section23.Section21exercise.Employee employee = new com.JGG.JDBC.Section23.Section21exercise.Employee(firstName, lastName, company);
+        //start a transaction
+        session.beginTransaction();
+        //save the  object
+        session.save(employee);
+        //commit transaction
+        session.getTransaction().commit();
+        System.out.println("Saved correctly: " + employee);
+
+    }
+
+    private static void retrieveEmployee() {
+        System.out.println("RETRIEVE A EMPLOYEE");
+
+        System.out.println("Insert the id");
+        int id = Integer.parseInt(scanner.nextLine());
+
+        session = factory.getCurrentSession();
+        //start a transaction
+        session.beginTransaction();
+        com.JGG.JDBC.Section23.Section21exercise.Employee employee = session.get(com.JGG.JDBC.Section23.Section21exercise.Employee.class, id);
+        System.out.println("Retrieved correctly: " + employee);
+        session.getTransaction().commit();
+    }
+
+    private static void queryEmployees() {
+        System.out.println("QUERY EMPLOYEES");
+
+        System.out.println("Insert the company name");
+        String company = scanner.nextLine();
+
+        session = factory.getCurrentSession();
+        session.beginTransaction();
+        List<com.JGG.JDBC.Section23.Section21exercise.Employee> employees=session.createQuery("from Employee where company='"+company+"'").getResultList();
+        System.out.println("Retrieved correctly: " + employees);
+        session.getTransaction().commit();
+    }
+
+    private static void deleteEmployee() {
+
+        System.out.println("DELETE A EMPLOYEE");
+
+        System.out.println("Insert the id");
+        int id = Integer.parseInt(scanner.nextLine());
+
+        session = factory.getCurrentSession();
+        session.beginTransaction();
+        session.createQuery("delete from Employee where id="+id).executeUpdate();
+        session.getTransaction().commit();
+        System.out.println("Deleted correctly");
+    }
+
+
+}
---
+++ S19_/src/com/JGG/JDBC/Section23/TestJdbcInstructor.java
@@ -0,0 +1,19 @@
+package com.JGG.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+        try{
+            System.out.println("Connecting to database: "+jdbcURL);
+            Connection connection= DriverManager.getConnection(jdbcURL, user, pass);
+            System.out.println("Success");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
