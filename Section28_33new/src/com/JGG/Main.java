cdb201ff3828df4f0a1c6ca57537f4a4cd5e2095
JGG <javieron.garcia@gmail.com>
2020-07-29 14:05:17 +0200

    Section 28. Part 1

---
+++ Section28_33new/src/com/JGG/Main.java
@@ -0,0 +1,9 @@
+package com.JGG;
+
+import com.JGG.testDB.TestDBServlet;
+
+public class Main {
+    public static void main(String[] args) {
+        TestDBServlet testDBServlet = new TestDBServlet();
+    }
+}
