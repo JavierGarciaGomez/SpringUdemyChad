b5571f6558b537c4c181f39c158f761ff59cc749
JGG <javieron.garcia@gmail.com>
2020-07-29 18:31:05 +0200

    Section 29. Part 1

--- Section28_33new/src/com/JGG/controller/CustomerController.java
+++ Section28_33new/src/com/JGG/controller/CustomerController.java
@@ -1,4 +1,4 @@
-package com.JGG.controller.entity;
+package com.JGG.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ Section28_33new/src/com/JGG/dao/CustomerDAO.java
@@ -0,0 +1,8 @@
+package com.JGG.dao;
+
+import com.JGG.controller.entity.Customer;
+import java.util.List;
+
+public interface CustomerDAO {
+    public List<Customer> getCustomers();
+}
