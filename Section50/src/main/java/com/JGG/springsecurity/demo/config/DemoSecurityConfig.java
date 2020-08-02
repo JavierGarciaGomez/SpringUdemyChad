c844c18917db585b3e35135dccd8098f062e3460
JGG <javieron.garcia@gmail.com>
2020-08-03 00:50:09 +0200

    Section 50-Spring Security - Logout

---
+++ Section50/src/main/java/com/JGG/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.JGG.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+//388, 395, 406
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Add our users for in memory authentication
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"));
+        auth.inMemoryAuthentication().withUser(users.username("mary").password("test123").roles("MANAGER"));
+        auth.inMemoryAuthentication().withUser(users.username("susan").password("test123").roles("ADMIN"));
+    }
+
+    //395, 406
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests() // Restrict access based on the HttpServletRequest
+                .anyRequest()
+                .authenticated() // any request must be authenticated
+                .and()
+                .formLogin() // customization formlogin
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+                .and() // added in 406
+                .logout()
+                .permitAll();
+    }
+}
