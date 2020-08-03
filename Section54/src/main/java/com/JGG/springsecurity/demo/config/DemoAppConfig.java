805aa084ae6f082e14424723e9c42c1e0f29756b
JGG <javieron.garcia@gmail.com>
2020-08-03 17:41:04 +0200

    Section 53-Spring Security - Restrict access based on role. Part 2

--- Section54/src/main/java/com/JGG/springsecurity/demo/config/DemoAppConfig.java
+++ Section54/src/main/java/com/JGG/springsecurity/demo/config/DemoAppConfig.java
@@ -1,18 +1,35 @@
 package com.JGG.springsecurity.demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-//380
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+//380, 432
 
 @Configuration
 @EnableWebMvc//similar support to <mvc:annotation-driven>
 @ComponentScan(basePackages = "com.JGG.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties") //432
 public class DemoAppConfig {
+    //433¡2 set up variable to hold the properties
+    @Autowired
+    private Environment environment; // hold the data read from properties files
+
+    // set up a logger for diagnostics
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+
     //380 Define a bean for View Resolver
     @Bean
     public ViewResolver viewResolver(){
@@ -22,5 +39,41 @@ public class DemoAppConfig {
 
         return viewResolver;
     }
+
+    //432 Define a bean for our security datasource
+    @Bean
+    public DataSource securityDataSource(){
+        //432 create connection pool
+        ComboPooledDataSource securityDataDource = new ComboPooledDataSource();
+        // 432 set the jdbc  driver class
+        try {
+            securityDataDource.setDriverClass(environment.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        //432 log the connection props
+        logger.info("432 >>> jdbc.url "+environment.getProperty("jdbc.url"));
+        logger.info("432 >>> jdbc.user "+environment.getProperty("jdbc.user"));
+        //434set database connection props
+        securityDataDource.setJdbcUrl(environment.getProperty("jdbc.url"));
+        securityDataDource.setUser(environment.getProperty("jdbc.user"));
+        securityDataDource.setPassword(environment.getProperty("jdbc.password"));
+        //434set connection pool props (READ IT FROM PROPERTIES)
+        securityDataDource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataDource.setMinPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataDource.setMaxPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataDource.setMaxIdleTime(getIntProperty("connection.pool.initialPoolSize"));
+        return securityDataDource;
+    }
+
+    //need a helper method to read environment property and convert to int
+    private int getIntProperty(String propName){
+        String propVal= environment.getProperty(propName);
+        //convert to Int
+        int intPropVal = Integer.parseInt(propVal);
+        return intPropVal;
+    }
+
+
 
 }
--- Section54/src/main/java/com/JGG/springsecurity/demo/config/DemoSecurityConfig.java
+++ Section54/src/main/java/com/JGG/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.JGG.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,18 +8,27 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
-//388, 395, 406, 418
+import javax.sql.DataSource;
+
+//388, 395, 406, 418, 435
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+    //435 Add a reference to our security data source
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+
+        /* Replaced in 435
         // Add our users for in memory authentication
         User.UserBuilder users = User.withDefaultPasswordEncoder();
         // 413 Adding more roles
         auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"));
         auth.inMemoryAuthentication().withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"));
-        auth.inMemoryAuthentication().withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+        auth.inMemoryAuthentication().withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));*/
     }
 
     //395, 406, 418
