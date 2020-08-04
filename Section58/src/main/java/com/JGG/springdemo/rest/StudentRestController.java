078215e1cd961cae9241b2beb10bbf4a41efcc3d
JGG <javieron.garcia@gmail.com>
2020-08-05 00:11:02 +0200

    Section 59-Spring REST - Spring REST. Retrieve POJO as JSON

---
+++ Section58/src/main/java/com/JGG/springdemo/rest/StudentRestController.java
@@ -0,0 +1,25 @@
+package com.JGG.springdemo.rest;
+
+import com.JGG.springdemo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//465
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    // define endpoint for "/students" - return students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+
+        return students;
+    }
+}
