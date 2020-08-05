306b93833588073bbff096e2a1575b6b00cf87d6
JGG <javieron.garcia@gmail.com>
2020-08-05 02:16:32 +0200

    Section 61-Spring REST - Exception Handling

--- Section58/src/main/java/com/JGG/springdemo/rest/StudentRestController.java
+++ Section58/src/main/java/com/JGG/springdemo/rest/StudentRestController.java
@@ -1,10 +1,9 @@
 package com.JGG.springdemo.rest;
 
 import com.JGG.springdemo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -20,7 +19,7 @@ public class StudentRestController {
 
     //468 Definie @PostConstruct to load the student data ... only once!
     @PostConstruct
-    public void loadData(){
+    public void loadData() {
         students = new ArrayList<>();
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
@@ -29,13 +28,43 @@ public class StudentRestController {
 
     // define endpoint for "/students" - return students
     @GetMapping("/students")
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return students;
     }
 
     // 469. define endpoint for "/student{studentId}" - return just one student
     @GetMapping("/students/{studentId}")
-    public Student getStudent(@PathVariable int studentId){
+    public Student getStudent(@PathVariable int studentId) {
+        // just index into the list
+        // 473 check the studentId against the list
+        if (studentId >= students.size() || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - "+studentId);
+        }
         return students.get(studentId);
     }
+
+/* Removed in 478, and pasted in StudentRestExceptionHandler as a Global Exception Handler
+   // 474. Add an exceptionhandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException (StudentNotFoundException studentNotFoundException){
+        //474 create a StudentErrorResponse
+        StudentErrorResponse studentErrorResponse = new StudentErrorResponse();
+        studentErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        studentErrorResponse.setMessage(studentNotFoundException.getMessage());
+        studentErrorResponse.setTimeStamp(System.currentTimeMillis());
+        // return responseEntity
+        return new ResponseEntity<>(studentErrorResponse, HttpStatus.NOT_FOUND);
+    }
+    //476 Add another exceptionHandler for other exceptions
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException (Exception e){
+        //474 create a StudentErrorResponse
+        StudentErrorResponse studentErrorResponse = new StudentErrorResponse();
+        studentErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value()); // Bad request
+        studentErrorResponse.setMessage(e.getMessage());
+        studentErrorResponse.setTimeStamp(System.currentTimeMillis());
+        // return responseEntity
+        return new ResponseEntity<>(studentErrorResponse, HttpStatus.BAD_REQUEST);
+    }*/
+
 }
--- Section58/src/main/java/com/JGG/springdemo/rest/StudentRestExceptionHandler.java
+++ Section58/src/main/java/com/JGG/springdemo/rest/StudentRestExceptionHandler.java
@@ -1,4 +1,35 @@
 package com.JGG.springdemo.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+//478
+@ControllerAdvice
 public class StudentRestExceptionHandler {
+    //Add exception handling code
+    
+    // 474. Add an exceptionhandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException (StudentNotFoundException studentNotFoundException){
+        //474 create a StudentErrorResponse
+        StudentErrorResponse studentErrorResponse = new StudentErrorResponse();
+        studentErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        studentErrorResponse.setMessage(studentNotFoundException.getMessage());
+        studentErrorResponse.setTimeStamp(System.currentTimeMillis());
+        // return responseEntity
+        return new ResponseEntity<>(studentErrorResponse, HttpStatus.NOT_FOUND);
+    }
+    //476 Add another exceptionHandler for other exceptions
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException (Exception e){
+        //474 create a StudentErrorResponse
+        StudentErrorResponse studentErrorResponse = new StudentErrorResponse();
+        studentErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value()); // Bad request
+        studentErrorResponse.setMessage(e.getMessage());
+        studentErrorResponse.setTimeStamp(System.currentTimeMillis());
+        // return responseEntity
+        return new ResponseEntity<>(studentErrorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
